5cf5cf10a5aa974e20d9a128fda5c48b836ad662
jrobson1065 <j.robson1065@gmail.com>
2020-06-19 17:46:21 -0400

    update travis

--- travis.js
+++ travis.js
@@ -1,12 +1,27 @@
-var character = document.getElementById('character');
-var block = document.getElementsByClassName('block');
+var character = document.getElementById("character");
+var block = document.getElementsByClassName("block");
 
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-        character.classList.add('jump');
+document.body.onkeyup = function (e) {
+  if (character.classList != "jump" && character.classList != "crouch") {
+    if (e.keyCode == 32 || e.keyCode == 40) {
+      jump();
     }
-}
+    if (e.keyCode == 40) {
+      crouch();
+    }
+  }
+};
 
 function jump() {
-    character.classList.add('jump');
+  character.classList.add("jump");
+  setTimeout(function () {
+    character.classList.remove("jump");
+  }, 1000);
+}
+
+function crouch() {
+  character.classList.add("crouch");
+  setTimeout(function () {
+    character.classList.remove("crouch");
+  }, 1000);
 }
