adc14df3f93172235312dce9a73d450cedeaa4fc
jrobson1065 <j.robson1065@gmail.com>
2020-07-21 17:02:58 -0400

    shows quote on open instead of waiting

--- cam/cam.js
+++ cam/cam.js
@@ -1,6 +1,9 @@
 const quotes = document.querySelectorAll(".quote");
 
-let n = 0;
+let n = 1;
+
+quotes[0].style.opacity = "1";
+
 
 setInterval(()=> {
     for (let i = 0; i < quotes.length; i++) quotes[i].style.opacity = "0";
