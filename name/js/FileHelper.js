cd6550b0188ad31e53e6d55655d17691d3444267
jrobson1065 <j.robson1065@gmail.com>
2020-08-18 17:15:00 -0400

    removed project

--- name/js/FileHelper.js
+++
@@ -1,8 +0,0 @@
-export class FileHelper {
-    pullTextFromFile = function(path) {
-        const request = new XMLHttpRequest();
-        request.open("GET", path, false);
-        request.send(null);
-        return request.responseText;
-    }
-}
